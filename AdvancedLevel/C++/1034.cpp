2084ec7928f6193d8871798fae8c91d0ea582fc7
Hunter <hspecial@163.com>
2020-04-02 16:41:19 +0800

    ::tada:: init commit A: 1034, 1076

---
+++ AdvancedLevel/C++/1034.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+int G[2000][2000] = {0}, weight[2000] = {0}; //邻接矩阵、点权数组
+int N, K, cnt = 0;
+string name[2000]; //编号->姓名
+int vis[2000] = {0}; //标记是否被访问过
+map<string, int> stringToInt; //姓名->编号
+map<string, int> gang; //头目->帮派人数
+
+int getId(string str) {
+	if(stringToInt.find(str) != stringToInt.end()) //当前姓名已有记录
+		return stringToInt[str];
+	stringToInt[str] = cnt; //记录姓名对应编号
+	name[cnt] = str; //记录编号对应姓名
+	return cnt++;
+}
+
+void DFS(int nowVisit, int &head, int &memCnt, int &totalW) {
+	memCnt++;
+	vis[nowVisit] = 1; //标记当前结点已访问
+	if(weight[nowVisit] > weight[head])
+		head = nowVisit; //更新头目
+	for(int i = 0; i < cnt; i++) { //枚举所有人
+		if(G[nowVisit][i] > 0) { //有直接联络的帮派成员
+			totalW += G[nowVisit][i]; //更新总的边权
+			G[nowVisit][i] = G[i][nowVisit] = 0; //删除边，防止回头
+			if(vis[i] == 0) //结点i未被访问，则递归访问
+				DFS(i, head, memCnt, totalW);
+		}
+	}
+}
+
+void DFSTrave() {
+	for(int i = 0; i < cnt; i++) { //枚举所有人
+		if(vis[i] == 0) {//未被访问过
+			int head = i, memCnt = 0, totalW = 0; //初始化头目，成员数量，图的总边权
+			DFS(i, head, memCnt, totalW); //遍历i所在的连通块(整个帮派)
+			if(memCnt > 2 && totalW > K) //成员数量 > 2, 总边权 > K，则为帮派
+				gang[name[head]] = memCnt; //记录帮派头目及成员数量
+		}
+	}
+}
+
+int main() {
+	cin >> N >> K;
+	string str1, str2;
+	int time;
+	for(int i = 0; i < N; i++) {
+		cin >> str1 >> str2 >> time;
+		int id1 = getId(str1); //将姓名转换为编号
+		int id2 = getId(str2);
+		weight[id1] += time; //统计点权
+		weight[id2] += time;
+		G[id1][id2] += time; //统计边权
+		G[id2][id1] += time;
+	}
+	DFSTrave(); //遍历整个图的连通块，获取帮派信息
+	cout << gang.size() << endl; //帮派个数
+	for(auto it = gang.begin(); it != gang.end(); it++) //遍历输出帮派的头目和人数
+		cout << it->first << " " << it->second << endl;
+	return 0;
+}
---
+++ AdvancedLevel/C++/1076.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+struct Node {
+	int id;
+	int layer; //结点层号
+};
+
+vector<Node> adj[1001]; //邻接表
+int inQ[1001] = {0}; //结点是否已加入过队列
+
+int BFS(int startId, int L) { //起始结点，转发层数上限
+	int numForward = 0; //转发数
+	queue<Node> q;
+	Node start = {startId, 0}; //定义起始结点
+	q.push(start);
+	inQ[startId] = 1;
+	while(!q.empty()) {
+		Node topNode = q.front(); //取出队首元素
+		q.pop();
+		int id = topNode.id;
+		for(int i = 0; i < adj[id].size(); i++) {
+			Node next = adj[id][i];
+			next.layer = topNode.layer + 1;
+			if(inQ[next.id] == 0 && next.layer <= L) {
+				q.push(next); //将next入队
+				inQ[next.id] = 1;
+				numForward++; //转发数 + 1
+			}
+		}
+	}
+	return numForward;
+}
+
+int main() {
+	Node user;
+	int N, L, cntFollow, idFollow;
+	cin >> N >> L;
+	for(int i = 1; i <= N; i++) {
+		user.id = i;
+		cin >> cntFollow; //关注的人数
+		for(int j = 0; j < cntFollow; j++) {
+			cin >> idFollow;
+			adj[idFollow].push_back(user); //建立关注者与被关注者的连接
+		}
+	}
+	int K, startId;
+	cin >> K; //查询个数
+	for(int i = 0; i < K; i++) {
+		fill(inQ, inQ + 1001, 0);
+		cin >> startId;
+		int layer = BFS(startId, L); //返回转发层数
+		cout << layer << endl;
+	}
+	return 0;
+}
